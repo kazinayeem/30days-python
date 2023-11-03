2fd5f782957cc46ba49aa9cfce650b99db27c2f5
kazinayeem <mohammadalinayeem360@gmail.com>
2023-11-04 01:26:41 +0600

    print

--- 1st-day.py
+++ 1st-day.py
@@ -2,3 +2,30 @@
 print("Hello world")
 
 print("hi my name is kazi nayeem")
+
+
+# Variables in python
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#Casting
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+# get the type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+# Case-Sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
